b755933cfb91480bc387eb775bf120c6ad3b9821
ramza81 <bandiera81@gmail.com>
2022-05-27 21:48:28 +0900

    Update Main.java

--- src/main/java/com/bandiera/javaoop/Main.java
+++ src/main/java/com/bandiera/javaoop/Main.java
@@ -6,8 +6,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
-
-        System.out.println("[result] " + sort.sort(Arrays.asList(args)));
+        System.out.println("hello world");
     }
 }
